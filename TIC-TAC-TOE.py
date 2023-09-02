145a27da926719305f8545f9669a14144cfa1cae
Neeraj Patil <140810217+NeerajPatil07@users.noreply.github.com>
2023-09-02 12:42:02 +0530

    Add files via upload

---
+++ TIC-TAC-TOE.py
@@ -0,0 +1,74 @@
+import random
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+def is_board_full(board):
+    return all(cell != " " for row in board for cell in row)
+
+def get_empty_cells(board):
+    empty_cells = []
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == " ":
+                empty_cells.append((row, col))
+    return empty_cells
+
+def ai_move(board):
+    empty_cells = get_empty_cells(board)
+    return random.choice(empty_cells)
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+
+        if current_player == "X":
+            while True:
+                try:
+                    row = int(input("Enter row (0-2): "))
+                    col = int(input("Enter column (0-2): "))
+                    if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == " ":
+                        break
+                    else:
+                        print("Invalid input. Try again.")
+                except ValueError:
+                    print("Invalid input. Enter numbers.")
+        else:
+            print("AI's turn")
+            row, col = ai_move(board)
+
+        board[row][col] = current_player
+
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        elif is_board_full(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        current_player = "O" if current_player == "X" else "X"
+
+if __name__ == "__main__":
+    main()
+
